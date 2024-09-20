a6ee42496b3608ee2af8fcba3712dc75cf3214b7
Thufail-Al-Humaidy <justtholib05@gmail.com>
2024-09-20 17:06:23 +0700

    second commit

--- script.js
+++ script.js
@@ -30,3 +30,33 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 });
+// about
+
+  // Scroll Animation Effect
+  window.addEventListener('scroll', function() {
+    const aboutSection = document.querySelector('.about');
+    const aboutPosition = aboutSection.getBoundingClientRect().top;
+    const screenPosition = window.innerHeight / 1.3;
+
+    if (aboutPosition < screenPosition) {
+      aboutSection.classList.add('show');
+    } else {
+      aboutSection.classList.remove('show');
+    }
+  });
+
+
+
+// JS FAQ's
+function toggleAnswer(button) {
+      const answerDiv = button.closest('.question').querySelector('.answer');
+      
+      if (answerDiv.style.height) {
+        // If already open, hide it
+        answerDiv.style.height = null;
+      } else {
+        // Get the full height of the content and set it
+        const fullHeight = answerDiv.scrollHeight + "px";
+        answerDiv.style.height = fullHeight;
+      }
+    }
