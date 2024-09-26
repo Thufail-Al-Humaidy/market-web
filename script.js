75ffb79cde8c5f366909b6dc48c84a5ce7a3fc9d
Thufail-Al-Humaidy <justtholib05@gmail.com>
2024-09-26 17:06:03 +0700

    membuat page products dan detail products

--- script.js
+++ script.js
@@ -4,46 +4,21 @@ document.getElementById("menubtn").addEventListener("click", function () {
   mobileMenu.classList.toggle("scale-y-100");
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-  let portfolioContainer = document.querySelector('.portfolio-container');
-  if (portfolioContainer) {
-    let portfolioIsotope = new Isotope(portfolioContainer, {
-      itemSelector: '.portfolio-item',
-      layoutMode: 'fitRows'
-    });
 
-    let portfolioFilters = document.querySelectorAll('#portfolio-flters li');
-    portfolioFilters.forEach((filter) => {
-      filter.addEventListener('click', function (e) {
-        e.preventDefault();
-        portfolioFilters.forEach(function (el) {
-          el.classList.remove('filter-active');
-          el.classList.replace('bg-green-500', 'bg-gray-500');
-        });
-        this.classList.add('filter-active');
-        this.classList.replace('bg-gray-500', 'bg-green-500');
-
-        portfolioIsotope.arrange({
-          filter: this.getAttribute('data-filter')
-        });
-      });
-    });
-  }
-});
 // about
 
   // Scroll Animation Effect
-  window.addEventListener('scroll', function() {
-    const aboutSection = document.querySelector('.about');
-    const aboutPosition = aboutSection.getBoundingClientRect().top;
-    const screenPosition = window.innerHeight / 1.3;
+  // window.addEventListener('scroll', function() {
+  //   const aboutSection = document.querySelector('.about');
+  //   const aboutPosition = aboutSection.getBoundingClientRect().top;
+  //   const screenPosition = window.innerHeight / 1.3;
 
-    if (aboutPosition < screenPosition) {
-      aboutSection.classList.add('show');
-    } else {
-      aboutSection.classList.remove('show');
-    }
-  });
+  //   if (aboutPosition < screenPosition) {
+  //     aboutSection.classList.add('show');
+  //   } else {
+  //     aboutSection.classList.remove('show');
+  //   }
+  // });
 
 
 
